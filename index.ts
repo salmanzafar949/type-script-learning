fd39f8f7cd8dd63f12c9ae8d585db5805fc49098
Salman Zafar <salman.zafar@innowi.com>
2021-01-01 16:09:00 +0500

    datatype

--- index.ts
+++ index.ts
@@ -1,1 +1,18 @@
-alert('hello world');
+// Data Types
+
+// Boolean
+var flag:boolean = true;
+
+// Number
+var num:number  = 12;
+
+// String
+var str:string = "abc";
+
+// array of number
+var arr:number[] = [1,2,3];
+
+// array if any thing e.g 1, 3, "a"
+var arr1:any[] = [1,2,3, "aa"];
+
+// alert('hello world');
